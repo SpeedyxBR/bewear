2f4518760dcaac74f059eea247384e4bdb623147
SpeedyxBR <andrel.cilva57@gmail.com>
2025-08-14 21:26:18 -0300

    feat: add partner list and update banner image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import CategorySelector from "@/components/common/category-selector";
 import Footer from "@/components/common/footer";
 import { Header } from "@/components/common/header";
+import { PartnersList } from "@/components/common/partners-list";
 import { ProductList } from "@/components/common/product-list";
 import { db } from "@/db";
 import { productTable } from "@/db/schema";
@@ -30,13 +31,20 @@ export default async function Home() {
       <div className="space-y-6">
         <div className="px-5">
           <Image
-            src="/banner-01.png"
+            src="/banner-01.svg"
             alt="Leve uma vida com estilo"
             height={0}
             width={0}
             sizes="100vw"
             className="h-auto w-full"
           />
+        </div>
+
+        <div className="px-5">
+          <div className="lg:col-span-3">
+            <PartnersList />
+          </div>
+
         </div>
         <ProductList products={products} title="Mais vendidos" />
         <div className="px-5">
