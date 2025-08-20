f10e6219b68968d029d64abc75bdec6fbcf61505
SpeedyxBR <andrel.cilva57@gmail.com>
2025-08-20 15:20:34 -0300

    feat: refatorar configuração de autenticação
    - Adiciona baseURL para configuração do Better Auth
    - Organiza a estrutura de autenticação, incluindo provedores sociais e configurações de banco de dados
    - Melhora a legibilidade do código ao formatar corretamente as importações e a estrutura do objeto de autenticação

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,32 +2,33 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { db } from "@/db"; // your drizzle instance
-import * as schema from '@/db/schema'
+import * as schema from "@/db/schema";
 
 export const auth = betterAuth({
-    emailAndPassword: {
-        enabled: true
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
-    socialProviders: {
-        google: {
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        },
-    },
-    database: drizzleAdapter(db, {
-        provider: "pg", // or "mysql", "sqlite"
-        schema
-    }),
-    user: {
-        modelName: "userTable"
-    },
-    session: {
-        modelName: "sessionTable"
-    },
-    account: {
-        modelName: "accountTable"
-    },
-    verification: {
-        modelName: "verificationTable",
-    },
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "mysql", "sqlite"
+    schema,
+  }),
+  user: {
+    modelName: "userTable",
+  },
+  session: {
+    modelName: "sessionTable",
+  },
+  account: {
+    modelName: "accountTable",
+  },
+  verification: {
+    modelName: "verificationTable",
+  },
 });
